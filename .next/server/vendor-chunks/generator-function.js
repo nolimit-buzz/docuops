e5680d8aefd0fe315ae094cc5ba21aa3aa190889
Akpan joseph <syntaxdesigners@gmail.com>
2026-03-18 18:14:44 +0100

    feat: Generate Next.js build output including compiled pages, vendor chunks, webpack assets, and generated types.

---
+++ .next/server/vendor-chunks/generator-function.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/generator-function";
+exports.ids = ["vendor-chunks/generator-function"];
+exports.modules = {
+
+/***/ "(ssr)/./node_modules/generator-function/index.js":
+/*!**************************************************!*\
+  !*** ./node_modules/generator-function/index.js ***!
+  \**************************************************/
+/***/ ((module) => {
+
+eval("\n\n// eslint-disable-next-line no-extra-parens, no-empty-function\nconst cached = /** @type {GeneratorFunctionConstructor} */ (function* () {}.constructor);\n\n/** @type {import('.')} */\nmodule.exports = () => cached;\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHNzcikvLi9ub2RlX21vZHVsZXMvZ2VuZXJhdG9yLWZ1bmN0aW9uL2luZGV4LmpzIiwibWFwcGluZ3MiOiJBQUFhOztBQUViO0FBQ0EsMEJBQTBCLDhCQUE4QixtQkFBbUI7O0FBRTNFLFdBQVcsYUFBYTtBQUN4QiIsInNvdXJjZXMiOlsiQzpcXFVzZXJzXFxzeW50YXhcXERvY3VtZW50c1xccHJvamVjdHNcXHJlbWl4Xy1kaWdpY3JlZC12OVxcbm9kZV9tb2R1bGVzXFxnZW5lcmF0b3ItZnVuY3Rpb25cXGluZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIid1c2Ugc3RyaWN0JztcblxuLy8gZXNsaW50LWRpc2FibGUtbmV4dC1saW5lIG5vLWV4dHJhLXBhcmVucywgbm8tZW1wdHktZnVuY3Rpb25cbmNvbnN0IGNhY2hlZCA9IC8qKiBAdHlwZSB7R2VuZXJhdG9yRnVuY3Rpb25Db25zdHJ1Y3Rvcn0gKi8gKGZ1bmN0aW9uKiAoKSB7fS5jb25zdHJ1Y3Rvcik7XG5cbi8qKiBAdHlwZSB7aW1wb3J0KCcuJyl9ICovXG5tb2R1bGUuZXhwb3J0cyA9ICgpID0+IGNhY2hlZDtcblxuIl0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6WzBdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(ssr)/./node_modules/generator-function/index.js\n");
+
+/***/ })
+
+};
+;
